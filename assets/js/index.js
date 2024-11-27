1b94679440f50eaf90ba1bffe42ad205474fd64b
Murad Gurbanzada <gurbanzadamurad@gmail.com>
2024-11-27 15:29:54 +0400

    navbar updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 const progress = document.querySelector(".progress");
 const progressBar = document.getElementById("progress");
+const listButton = document.getElementById("list-button");
+const searchButton = document.getElementById("search-button");
+const underMenu = document.getElementById("under--menu");
 
 // when opening the website, closing loading components 
 window.onload = () => {
@@ -7,6 +10,9 @@ window.onload = () => {
       progress.classList.add("close");
    }, 1300);
 }
+
+handleOpenAndCloseBtn(listButton);
+handleOpenAndCloseBtn(searchButton);
 
 
 
@@ -21,6 +27,18 @@ const intervalid = setInterval(() => {
       clearInterval(intervalid);
       return;
    }
-   progressBar.style.setProperty('--width',width + 0.3);
-   
-}, 1);
+   progressBar.style.setProperty('--width',width + 0.5);
+}, 0.3);
+
+//  dynamic clicking open and close submenu
+function handleOpenAndCloseBtn(btn){
+   btn.addEventListener("click",() => {
+      if(underMenu.classList.contains("open")){
+         underMenu.classList.remove("open");
+         listButton.classList.remove("icon-remove")
+      }else{
+         underMenu.classList.add("open");
+         listButton.classList.add("icon-remove")
+      }
+   });
+}
